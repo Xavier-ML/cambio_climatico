9e116ce7389184ed99fb20385b9dd00ac476cd45
rstankovic <cschristianstankovic@gmail.com>
2020-10-20 18:33:05 -0500

    added analysis to notebook plus markdown

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 # import necessary libraries
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
 
-import config
-import lists
+import pymongo
+
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+from config import usnm, pswd
+from lists import series_list, country_list
 
 # create instance of Flask app
 app = Flask(__name__)
@@ -16,6 +22,13 @@ client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.iadr
 
 db = client.climate_db
 
+app.config['SECRET_KEY'] = 'usnmpswd'
+
+class SearchForm(FlaskForm):
+    cat_1 = StringField('Category 1', validators = [DataRequired()])
+    cat_2 = StringField('Category 2', validators = [DataRequired()])
+    submit = SubmitField('Search')
+
 # create route that renders index.html template
 @app.route("/")
 def index():
@@ -23,9 +36,10 @@ def index():
     ctgrs_list = ctgrs_list,
     cntr_list = cntr_list)
     
-@app.route("/visualizations")
+@app.route("/Visualizations", methods = ['GET','POST'])
 def visualize():
-    return render_template("visualizations.html")
+    form = SearchForm()
+    return render_template("visualizations.html", form=form)
 
 
 if __name__ == "__main__":
